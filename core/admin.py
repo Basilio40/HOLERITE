2ee90a0d4999669e2fc76442fc12a8b5561cb001
Basilio40 <42722896+Basilio40@users.noreply.github.com>
2021-12-06 22:04:44 -0300

    primeiro_commit

---
+++ core/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import FilePdf, Ponto, Holerite
+
+# Register your models here.
+admin.site.register(FilePdf)
+admin.site.register(Ponto)
+admin.site.register(Holerite)
---
+++ core/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class MixData(models.Model):
+    data_insercao = models.DateTimeField(
+        auto_now_add=True, verbose_name="Data de Registro"
+    )
+    data_modificacao = models.DateTimeField(
+        auto_now_add=True, verbose_name="Data de Modificação"
+    )
+    class Meta:
+        abstract = True
+
+class FilePdf(MixData):
+    caminho_arquivo = models.FileField()
+    nome_arquivo = models.CharField(max_length=100)
+
+
+class Holerite(FilePdf):
+    mes = models.DateField()
+    ano = models.IntegerField()    
+
+
+class Ponto(FilePdf):
+    mes_choices = (("Jan", "Janeiro"), ("Fev", "Feveiro"), ("Mar", "Março"),("Abr" , "Abril"),("Mai","Maio"),("Jun","Junho"),("Jul","Julho"),("Ago","Agosto"),("Set","Setembro"),
+                   ("Out","Outubro"),("Nov","Novembro"),("Dez","Dezembro"),)
+    mes = models.CharField(max_length=4, choices=mes_choices,
+                        default=mes_choices[0][0],)
+    ano = models.IntegerField()
+    
+    def __str__(self) :
+        return self.mes
+
+
+class Funcionario(MixData):
+    nome = models.CharField(max_length=100)
+    holerite = models.ForeignKey(Holerite, on_delete=models.CASCADE)
+    ponto = models.ForeignKey(Ponto, on_delete=models.CASCADE)
+
+@receiver(post_save, sender=Ponto)
+def split_pdf(instance, created, **kargs):
+    print(f"SALVANDO O {instance.caminho_arquivo}")
